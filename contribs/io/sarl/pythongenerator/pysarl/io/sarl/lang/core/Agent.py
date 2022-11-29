9ff69712911fd1fbe8b45fc17da0166c5f63a84f
homermafia <ahmetadam4@gmail.com>
2022-11-30 00:14:33 +0100

    Agent class rewrite

--- contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/Agent.py
+++ contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/Agent.py
@@ -1,29 +1,49 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  4 20:56:52 2022
+from typing import Callable
+from uuid import UUID
+from multipledispatch import dispatch
 
-@author: loic
-"""
-
+from pysarl.io.sarl.lang.core.Address import Address
 from pysarl.io.sarl.lang.core.AbstractSkillContainer import AbstractSkillContainer
+from pysarl.io.sarl.lang.core.DynamicSkillProvider import DynamicSkillProvider
+from pysarl.io.sarl.lang.core.Event import Event
+from pysarl.io.sarl.lang.core.IBehaviorGuardEvaluatorReceiver import IBehaviorGuardEvaluatorReceiver
+from pysarl.io.sarl.lang.core.Skill import Skill
 
 
-class Agent(AbstractSkillContainer):
+class Agent(AbstractSkillContainer, IBehaviorGuardEvaluatorReceiver):
+    __id: UUID
+    __parentID: UUID
+    __skillCallback: Callable[["Agent", Skill], None] = None
 
-    def __init__(self, parentID, agentID, dynamicSkillProvider = None):
+    def __init__(self, parentID: UUID, agentID: UUID, dynamicSkillProvider: DynamicSkillProvider = None):
         super(Agent, self).__init__(dynamicSkillProvider)
         self.__parentID = parentID
-        self.__agentID = agentID
-
-    def getID(self):
-        return self.__agentID
-
-    def getParentID(self):
-        return self.__parentID
+        self.__id = agentID if agentID is not None else UUID()
 
     def __str__(self) -> str:
-        return "Agent{ID:" + str(self.__agentID) + ", parentID:" + str(self.__parentID) + "}"
+        return "Agent{ID:" + str(self.__id) + ", parentID:" + str(self.__parentID) + "}"
 
-    def isMe(self, uID):
-        return uID is not None and uID == self.__agentID
+    def _attachOwner(self, skill: Skill) -> None:
+        skill.setOwner(self)
+        if self.__skillCallback is not None:
+            self.__skillCallback(self, skill)
 
+    def setSkillCallback(self, callback: Callable[["Agent", Skill], None]) -> None:
+        self.__skillCallback = callback
+
+    def getParentID(self) -> UUID:
+        return self.__parentID
+
+    def getID(self) -> UUID:
+        return self.__id
+
+    @dispatch(Address)
+    def isMe(self, address: Address) -> bool:
+        return address is not None and self.isMe(address.getID())
+
+    @dispatch(UUID)
+    def isMe(self, uID: UUID) -> bool:
+        return uID is not None and uID == self.__id
+
+    def isFromMe(self, event: Event) -> bool:
+        return event is not None and self.isMe(event.getSource())
