c83a280f022bc4a66e5c361fd3d42f70d6d40fca
homermafia <ahmetadam4@gmail.com>
2022-11-29 19:02:33 +0100

    AbstractSkillContainer rewrite

--- contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/Address.py
+++ contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/Address.py
@@ -1,24 +1,43 @@
-from contribs.io.sarl.pythongenerator.api.space.spaceID import SpaceID
-import uuid
+import copy
+from uuid import UUID
+
+from pysarl.io.sarl.lang.core.SRESpecificDataContainer import SRESpecificDataContainer
+from pysarl.io.sarl.lang.core.SpaceID import SpaceID
 
 
+class Address(SRESpecificDataContainer):
+    __serialVersionUID: int = 1519844913685586094
+    __participantId: UUID
+    __spaceId: SpaceID
 
-class Address(object):
+    def __init__(self, spaceId: SpaceID, participantId: UUID):
+        assert spaceId is not None
+        assert participantId is not None
 
-    def __init__(self, spaceId: SpaceID, participantId: uuid):
-        assert (spaceId is not None) and (participantId is not None)
-        self.__spaceId = spaceId
         self.__participantId = participantId
+        self.__spaceId = spaceId
 
-    def getParticipantId(self):
-        return self.__participantId
-
-    def getSpaceId(self):
-        return self.__spaceId
+    def clone(self) -> "Address":
+        # shallow copy
+        try:
+            return copy.copy(self)
+        except copy.Error as e:
+            raise Exception(e)
 
     def __str__(self) -> str:
         return "Address{" + str(self.__spaceId) + ", ParticipantId : " + str(self.__participantId) + "}"
 
-    def equals(self, address):
-        return (address is not None) and (self.__spaceId.equals(address.getSpaceId())) and\
-               (self.__participantId == address.getParticipantId())
+    def getUUID(self) -> UUID:
+        return self.getID()
+
+    def getID(self) -> UUID:
+        return self.__participantId
+
+    def equals(self, address: "Address") -> bool:
+        return (address is not None) and (self.__participantId == address.getID()) and (self.__spaceId.equals(address.getSpaceID()))
+
+    def getSpaceID(self) -> SpaceID:
+        return self.__spaceId
+
+    def getSpaceId(self) -> SpaceID:
+        return self.getSpaceID()
