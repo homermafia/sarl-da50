a03597559dd0629463bdf103c90039792d09345f
homermafia <ahmetadam4@gmail.com>
2022-12-03 11:00:56 +0100

    DeadEvent API written

--- contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/DeadEvent.py
+++ contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/DeadEvent.py
@@ -2,4 +2,12 @@ from pysarl.io.sarl.lang.core.Event import Event
 
 
 class DeadEvent(Event):
-    pass
+    __event: object
+
+    def __init__(self, event: Event):
+        assert event is not None
+        super().__init__(event.getSource())
+        self.__event = event
+
+    def getEvent(self) -> object:
+        return self.__event
