0967696d4ed0717ad16828f82c8a3c83655bcbbf
homermafia <ahmetadam4@gmail.com>
2022-12-03 11:08:24 +0100

    DefaultSkill API written

--- contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/DefaultSkill.py
+++ contribs/io/sarl/pythongenerator/pysarl/io/sarl/lang/core/DefaultSkill.py
@@ -1,2 +1,13 @@
-class DefaultSkill():
-    pass
+import abc
+from typing import TypeVar, Type
+
+from pysarl.io.sarl.lang.core.Skill import Skill
+
+S = TypeVar('S', bound=Type[Skill])
+
+
+class DefaultSkill(abc.ABC):
+
+    @abc.abstractmethod
+    def value(self) -> S:
+        pass
