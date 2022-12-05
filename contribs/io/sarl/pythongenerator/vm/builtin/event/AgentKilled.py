87cde5b03e090561ed91497bde719dc178dfa491
Yasmarila <58823553+Yasmarila@users.noreply.github.com>
2022-12-05 10:56:21 +0100

    "AgentKilled.py, AgentSpawned.py and KillMeException added"

---
+++ contribs/io/sarl/pythongenerator/vm/builtin/event/AgentKilled.py
@@ -0,0 +1,15 @@
+from contribs.io.sarl.pythongenerator.api.event.event import Event
+
+
+class AgentKilled(Event):
+
+    def __init__(self, sourceAddress, agentType: str, terminationCause = None):
+        super().__init__(sourceAddress)
+        self.__agentType = agentType
+        self.__terminationCause = terminationCause
+
+    def getAgentType(self):
+        return self.__agentType
+
+    def getTerminationCause(self):
+        return self.
