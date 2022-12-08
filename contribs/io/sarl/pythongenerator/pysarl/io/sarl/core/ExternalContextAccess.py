546abe690ce7bf6b9f1baf1f16c3b2f6a482b6b4
homermafia <ahmetadam4@gmail.com>
2022-12-08 18:27:19 +0100

    ExternalContextAccess API completed + DefaultContextInteractions corrections

--- contribs/io/sarl/pythongenerator/pysarl/io/sarl/core/ExternalContextAccess.py
+++ contribs/io/sarl/pythongenerator/pysarl/io/sarl/core/ExternalContextAccess.py
@@ -1,37 +1,50 @@
-from pysarl.io.sarl.lang.core.EventSpace import EventSpace
-from pysarl.io.sarl.lang.core.Capacity import Capacity
-from pysarl.io.sarl.lang.core.Event import Event
-from pysarl.io.sarl.lang.core.AgentContext import AgentContext
-from pysarl.io.sarl.lang.core.Space import Space
-from pysarl.io.sarl.lang.core.SpaceID import SpaceID
+from __future__ import annotations
+import abc
+from typing import List, TYPE_CHECKING
 from uuid import UUID
 
-class ExternalContextAccess(Capacity):
+from multipledispatch import dispatch
+
+if TYPE_CHECKING:
+    from pysarl.io.sarl.lang.core.Address import Address
+    from pysarl.io.sarl.lang.core.EventSpace import EventSpace
+    from pysarl.io.sarl.lang.core.AgentContext import AgentContext
+    from pysarl.io.sarl.lang.core.Scope import Scope
+
+from pysarl.io.sarl.lang.core.Capacity import Capacity
+from pysarl.io.sarl.lang.core.Event import Event
+from pysarl.io.sarl.lang.core.Space import Space
+from pysarl.io.sarl.lang.core.SpaceID import SpaceID
+
+
+class ExternalContextAccess(Capacity, abc.ABC):
+
     """
+    Emits an event with the provided scope in a space
     @param : space
     @param : event
     @param : scope
-    Emits an event with the provided scope in a space
     """
-
-    def emit(space: EventSpace, event: Event, scope):
+    @abc.abstractmethod
+    def emit(self, space: EventSpace, event: Event, scope: Scope[Address]) -> None:
         pass
 
-    def getAllContexts(self):
+    @abc.abstractmethod
+    def getAllContexts(self) -> List[AgentContext]:
         pass
 
     """
-    @param : contextID 
     Return the AgentContext for the contextID.
+    @param : contextID 
     """
-
+    @abc.abstractmethod
     def getContext(self, contextID: UUID) -> AgentContext:
         pass
 
     """
     Return the AgentContext that is the root of all the contexts
     """
-
+    @abc.abstractmethod
     def getUniverseContext(self) -> AgentContext:
         pass
 
@@ -40,7 +53,8 @@ class ExternalContextAccess(Capacity):
     @param : event
     @param : space
     """
-
+    @dispatch(Event, Space)
+    @abc.abstractmethod
     def isInSpace(self, event: Event, space: Space) -> bool:
         pass
 
@@ -49,11 +63,19 @@ class ExternalContextAccess(Capacity):
     @param : event
     @param : spaceID
     """
-
-    def isInSpaceID(self, event: Event, spaceID: SpaceID) -> bool:
+    @dispatch(Event, SpaceID)
+    @abc.abstractmethod
+    def isInSpace(self, event: Event, spaceID: SpaceID) -> bool:
         pass
 
-    def isInSpaceUUID(self, event: Event, spaceID: UUID) -> bool:
+    """
+    Replies if the given event was emitted in the space with the given identifier..
+    @param : event
+    @param : spaceID
+    """
+    @dispatch(Event, UUID)
+    @abc.abstractmethod
+    def isInSpace(self, event: Event, spaceID: UUID) -> bool:
         pass
 
     """
@@ -61,7 +83,7 @@ class ExternalContextAccess(Capacity):
     @param : contextID
     @param : expectedDefaultSpaceID
     """
-
+    @abc.abstractmethod
     def join(self, contextID: UUID, expectedDefaultSpaceID: UUID) -> AgentContext:
         pass
 
@@ -69,6 +91,6 @@ class ExternalContextAccess(Capacity):
     Leave the parent's context
     @param : contextID
     """
-
+    @abc.abstractmethod
     def leave(self, contextID: UUID) -> bool:
         pass
