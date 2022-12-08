048d8f59d175029b0bb372b4512c2c49e7db1447
homermafia <ahmetadam4@gmail.com>
2022-12-08 18:34:55 +0100

    InnerContextAccess API completed

--- contribs/io/sarl/pythongenerator/pysarl/io/sarl/core/InnerContextAccess.py
+++ contribs/io/sarl/pythongenerator/pysarl/io/sarl/core/InnerContextAccess.py
@@ -1,41 +1,51 @@
+from __future__ import annotations
+import abc
+from typing import Set, TYPE_CHECKING
+from uuid import UUID
+
+from multipledispatch import dispatch
+
+if TYPE_CHECKING:
+    from pysarl.io.sarl.lang.core.Event import Event
+    from pysarl.io.sarl.lang.core.AgentContext import AgentContext
+    from pysarl.io.sarl.lang.core.EventSpace import EventSpace
+
 from pysarl.io.sarl.lang.core.Capacity import Capacity
 from pysarl.io.sarl.lang.core.Space import Space
 from pysarl.io.sarl.lang.core.SpaceID import SpaceID
-from pysarl.io.sarl.lang.core.Event import Event
-from pysarl.io.sarl.lang.core.AgentContext import AgentContext
-from pysarl.io.sarl.lang.core.EventSpace import EventSpace
-from uuid import UUID
 
 
-class InnerContextAccess(Capacity):
+class InnerContextAccess(Capacity, abc.ABC):
+
     """
     Return the inner context.
     """
-
+    @abc.abstractmethod
     def getInnerContext(self) -> AgentContext:
         pass
 
+    @abc.abstractmethod
     def getInnerDefaultSpace(self) -> EventSpace:
         pass
 
     """
     Return the number of agent that are agent of the inner context
     """
-
+    @abc.abstractmethod
     def getMemberAgentCount(self) -> int:
         pass
 
     """
     Return all agent in the inner context
     """
-
-    def getMemberAgents(self):
+    @abc.abstractmethod
+    def getMemberAgents(self) -> Set[UUID]:
         pass
 
     """
     Return true if it has a member agent
     """
-
+    @abc.abstractmethod
     def hasMemberAgent(self) -> bool:
         pass
 
@@ -43,15 +53,16 @@ class InnerContextAccess(Capacity):
     Return true if the given event was emitted in the default space of the inner context
     @param : event
     """
-
-    def isInnerDefaultSpace(self, event: Event) -> bool:
+    @abc.abstractmethod
+    def isInInnerDefaultSpace(self, event: Event) -> bool:
         pass
 
     """
     Return true if the given space is the default space of the inner context
     @param : space
     """
-
+    @dispatch(Space)
+    @abc.abstractmethod
     def isInnerDefaultSpace(self, space: Space) -> bool:
         pass
 
@@ -59,14 +70,16 @@ class InnerContextAccess(Capacity):
     Replies if the given identifier is the identifier of the default space of the inner context.
     @param : spaceID
     """
-
-    def isInnerDefaultSpace(spaceID: SpaceID) -> bool:
+    @dispatch(SpaceID)
+    @abc.abstractmethod
+    def isInnerDefaultSpace(self, spaceID: SpaceID) -> bool:
         pass
 
     """
     Replies if the given identifier is the identifier of the default space of the inner context
     @param : spaceID
     """
-
-    def isInnerDefaultSpace(spaceID: UUID) -> bool:
+    @dispatch(UUID)
+    @abc.abstractmethod
+    def isInnerDefaultSpace(self, spaceID: UUID) -> bool:
         pass
