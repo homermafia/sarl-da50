387f60316c1e9b32d4ba36a924a8ed2368a9fb63
Zapolatero <pascuzzilouis@gmail.com>
2022-12-23 16:31:24 +0100

    Creation of a DefaultContext for each Agent and of an default EventSpace in each Context

--- contribs/io/sarl/pythongenerator/vm/builtin/service/Context.py
+++ contribs/io/sarl/pythongenerator/vm/builtin/service/Context.py
@@ -1,14 +1,14 @@
-from pysarl.io.sarl.lang.core.Space import Space
+from pysarl.io.sarl.lang.core.EventSpace import EventSpace
 from vm.builtin.service.PythonContext import PythonContext
 
 
 class Context(PythonContext):
     def __init__(self):
-        self.defaultSpace = Space()
+        self.defaultSpace = EventSpace()
         self.spaces = []
 
     def getDefaultSpace(self):
         return self.defaultSpace
 
     def createSpace(self, uuid):
-        self.spaces.append(Space())
+        self.spaces.append(EventSpace())
